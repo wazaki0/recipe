5b890bd98d5aadb74108d3718c83fc6096b8951a
Sebastian Ohajda <sebastian.ohajda@gmail.com>
2021-03-17 22:04:24 +0100

    auth part 1

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,57 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+
+  isLoginPage = true;
+  authForm: FormGroup;
+  // signupForm: FormGroup;
+  // loginForm: FormGroup;
 
   constructor() {
   }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  onSwitchmode(): void {
+    this.isLoginPage = !this.isLoginPage;
+    this.authForm = null;
+    this.initForm();
+  }
+
+  onSubmit(): void {
+    console.log(this.authForm);
+  }
+
+  private initForm(): void {
+
+    const email = '';
+    const password = '';
+    const username = '';
+
+    if (this.isLoginPage) {
+
+      this.authForm = new FormGroup({
+        email: new FormControl(password, Validators.required),
+        password: new FormControl(email, Validators.required)
+      });
+
+    } else {
+      this.authForm = new FormGroup({
+        email: new FormControl(email, [Validators.required, Validators.email]), // initialize, validator - rules before submitting
+        username: new FormControl(username, Validators.required),
+        password: new FormControl(password, Validators.required),
+      });
+    }
+
+
+  }
+
 }
